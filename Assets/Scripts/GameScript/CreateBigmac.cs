077e24bd50378acebafac74352986c64f113646e
Matsuel <matsuel0308@gmail.com>
2024-01-26 16:59:25 +0100

    add BOMBSSSSSS if random == 5, 5 spawns for 2 seconds

--- Assets/Scripts/GameScript/CreateBigmac.cs
+++ Assets/Scripts/GameScript/CreateBigmac.cs
@@ -11,14 +11,13 @@ public class CreateBigmac : MonoBehaviour
     private float maxY = 9.2f;
     private float minY = -9.2f;
     public bool isAlive = true;
-    public List<GameObject> bigmacs = new List<GameObject>();
-    public List<GameObject> bigmacsToDestroy = new List<GameObject>();
-
+    public List<GameObject> bombs = new List<GameObject>();
     public Sprite bigmacSprite;
     public Sprite bananaSprite;
     public Sprite cocacolaSprite;
     private List<Sprite> sprites = new List<Sprite>();
-    // Start is called before the first frame update
+    private bool isBomb = false;
+    
     void Start()
     {
         sprites.Add(bigmacSprite);
@@ -26,7 +25,6 @@ public class CreateBigmac : MonoBehaviour
         StartCoroutine(createBigmac());
     }
 
-    // Update is called once per frame
     void Update()
     {
 
@@ -34,31 +32,45 @@ public class CreateBigmac : MonoBehaviour
 
     public IEnumerator createBigmac()
     {
-        int randomMagic = Random.Range(0, 25);
-        if (randomMagic == 22 || randomMagic == 13)
+        int randomMagic = Random.Range(0, 8);
+        if (!isBomb && randomMagic == 5)
+        {
+            isBomb = true;
+            for (int i = 0; i < 5; i++)
+            {
+                GameObject newBigmac = Instantiate(sapeur);
+                newBigmac.transform.position = new Vector2(Random.Range(minX, maxX), Random.Range(minY, maxY));
+                bombs.Add(newBigmac);
+            }
+            yield return new WaitForSeconds(2f);
+            DestroyBombs();
+            isBomb = false;
+            StartCoroutine(createBigmac());
+        }
+        else if (randomMagic == 2 || randomMagic == 7 && !isBomb)
         {
             GameObject newBigmac = Instantiate(bigmac);
             newBigmac.tag = "Magic";
             newBigmac.GetComponent<SpriteRenderer>().sprite = bananaSprite;
             newBigmac.transform.position = new Vector2(Random.Range(minX, maxX), Random.Range(minY, maxY));
-            bigmacs.Add(newBigmac);
             yield return new WaitForSeconds(0.5f);
         }
-        else
+        else if (!isBomb)
         {
-            GameObject newBigmac = Instantiate(sapeur);
+            int random = Random.Range(0, 2);
+            GameObject newBigmac = Instantiate(bigmac);
+            newBigmac.GetComponent<SpriteRenderer>().sprite = sprites[random];
             newBigmac.transform.position = new Vector2(Random.Range(minX, maxX), Random.Range(minY, maxY));
-            bigmacs.Add(newBigmac);
             yield return new WaitForSeconds(0.5f);
         }
-        // else
-        // {
-        //     int random = Random.Range(0, 2);
-        //     GameObject newBigmac = Instantiate(bigmac);
-        //     newBigmac.GetComponent<SpriteRenderer>().sprite = sprites[random];
-        //     newBigmac.transform.position = new Vector2(Random.Range(minX, maxX), Random.Range(minY, maxY));
-        //     bigmacs.Add(newBigmac);
-        //     yield return new WaitForSeconds(0.5f);
-        // }
+    }
+
+    private void DestroyBombs()
+    {
+        foreach (GameObject bomb in bombs)
+        {
+            Destroy(bomb);
+        }
+        bombs.Clear();
     }
 }
